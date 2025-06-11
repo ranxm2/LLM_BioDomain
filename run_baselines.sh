f1238de3f8901ac7c7170d2154fce39fa9666599
Jiaying Lu <jiaying.lu@emory.edu>
2025-06-11 10:50:27 -0400

    add Graph Traversal baseline

--- run_baselines.sh
+++ run_baselines.sh
@@ -1,4 +1,5 @@
-uv run python -m baselines.random_ranker main
+# Random Ranker
+uv run python -m baselines.random_ranker main --seed 42
 uv run python -m baselines.eval eval
 
 uv run python -m baselines.random_ranker main --seed 27
@@ -11,4 +12,23 @@ uv run python -m baselines.random_ranker main --seed 2025
 uv run python -m baselines.eval eval \
   --result_path './Experiment/00-Baselines/random/seed_2025.json' \
   --leaderboard_path './Experiment/00-Baselines/random/seed_2025_leaderboard.csv' \
+  --method_name 'random_seed_2025'
+
+  # Graph Traversal
+  uv run python -m baselines.graph_traversal main --seed 42
+  uv run python -m baselines.eval eval \
+  --result_path './Experiment/00-Baselines/graph_traversal/seed_42.json' \
+  --leaderboard_path './Experiment/00-Baselines/graph_traversal/seed_42_leaderboard.csv' \
+  --method_name 'random_seed_42'
+
+  uv run python -m baselines.graph_traversal main --seed 27
+  uv run python -m baselines.eval eval \
+  --result_path './Experiment/00-Baselines/graph_traversal/seed_27.json' \
+  --leaderboard_path './Experiment/00-Baselines/graph_traversal/seed_27_leaderboard.csv' \
+  --method_name 'random_seed_27'
+
+  uv run python -m baselines.graph_traversal main --seed 2025
+  uv run python -m baselines.eval eval \
+  --result_path './Experiment/00-Baselines/graph_traversal/seed_2025.json' \
+  --leaderboard_path './Experiment/00-Baselines/graph_traversal/seed_2025_leaderboard.csv' \
   --method_name 'random_seed_2025'
